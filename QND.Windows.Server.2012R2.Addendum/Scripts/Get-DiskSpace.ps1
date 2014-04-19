770e427ee8c408a5707a8a6831f5a30bd5884f5a
Daniele Grandini <daniele@grandini.com>
2014-04-19 17:56:00 +0200

    Added not working version of addendum for 2012 R2

---
+++ QND.Windows.Server.2012R2.Addendum/Scripts/Get-DiskSpace.ps1
@@ -0,0 +1,10 @@
+﻿parameter($P_DiskLabel, $TargetComputer )
+
+$disk = gwmi -computer $TargetComputer -query "select * from Win32_volume where Name = '$P_DiskLabel\\' or DriveLetter='$P_DiskLabel' or DeviceId='$P_DiskLabel'"
+$result = @"
+Server: $TargetComputer. Disk: $P_DiskLabel
+Disk Size (MB): $($disk.Capacity/(1024*1024))
+Disk Free Space (MB): $($disk.FreeSpace/(1024*1024))
+Disk Free Space (%): $($disk.FreeSpace/$Disk.Capacity*100)
+"@
+Write-Output $result
