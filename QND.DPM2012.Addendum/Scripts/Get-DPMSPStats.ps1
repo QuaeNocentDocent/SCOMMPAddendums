ada6afa00fc7b7bed74f52021dd3c8028d5aace1
Brandubh <daniele@grandini.com>
2014-08-13 10:27:59 +0200

    Added QND.DPM2012.Addendum

---
+++ QND.DPM2012.Addendum/Scripts/Get-DPMSPStats.ps1
@@ -0,0 +1,129 @@
+﻿
+
+#*************************************************************************
+# Script Name - 
+# Author	  -  - Progel srl
+# Version	  - 1.1 24.09.2007
+# Purpose     - 
+#               
+# Assumptions - 
+#				
+#               
+# Parameters  - TraceLevel
+#             - ComputerName
+#				- SourceId
+#				- ManagedEntityId
+# Command Line - .\test.ps1 4 "serverName" '{1860E0EB-8C21-41DA-9F35-2FE9343CCF36}' '{1860E0EB-8C21-41DA-9F35-2FE9343CCF36}'
+# If discovery must be added the followinf parameters
+#				SourceId ($MPElement$ )
+#				ManagedEntityId ($Target/Id$)
+#
+# Output properties
+#
+# Status
+#
+# Version History
+#	  1.0 06.08.2010 DG First Release
+#     1.5 15.02.2014 DG minor cosmetics
+#
+# (c) Copyright 2010, Progel srl, All Rights Reserved
+# Proprietary and confidential to Progel srl              
+#
+#*************************************************************************
+
+
+# Get the named parameters
+param([int]$traceLevel=$(throw 'must have a value'))
+
+	[Threading.Thread]::CurrentThread.CurrentCulture = "en-US"        
+    [Threading.Thread]::CurrentThread.CurrentUICulture = "en-US"
+	
+#Constants used for event logging
+$SCRIPT_NAME			= "Get-DPMSPStats"
+$SCRIPT_ARGS = 1
+
+$SCRIPT_STARTED			= 831
+$PROPERTYBAG_CREATED	= 832
+$SCRIPT_ENDED			= 835
+
+$SCRIPT_VERSION = "1.0"
+
+#Trace Level Costants
+$TRACE_NONE 	= 0
+$TRACE_ERROR 	= 1
+$TRACE_WARNING = 2
+$TRACE_INFO 	= 3
+$TRACE_VERBOSE = 4
+$TRACE_DEBUG = 5
+
+#Event Type Constants
+$EVENT_TYPE_SUCCESS      = 0
+$EVENT_TYPE_ERROR        = 1
+$EVENT_TYPE_WARNING      = 2
+$EVENT_TYPE_INFORMATION  = 4
+$EVENT_TYPE_AUDITSUCCESS = 8
+$EVENT_TYPE_AUDITFAILURE = 16
+
+#Standard Event IDs
+$FAILURE_EVENT_ID = 4000		#errore generico nello script
+$SUCCESS_EVENT_ID = 1101
+$START_EVENT_ID = 1102
+$STOP_EVENT_ID = 1103
+
+#TypedPropertyBag
+$AlertDataType = 0
+$EventDataType	= 2
+$PerformanceDataType = 2
+$StateDataType       = 3
+
+function Log-Params
+{
+    $line=''
+    foreach($key in $MyInvocation.BoundParameters.Keys) {$line += "$key=$($MyInvocation.BoundParameters[$key])  "}
+	Log-Event $START_EVENT_ID $EVENT_TYPE_INFORMATION  ("Starting script. Invocation Name:$($MyInvocation.InvocationName)`n Parameters`n $line") $TRACE_INFO
+}
+
+function Log-Event
+{
+	param($eventID, $eventType, $msg, $level)
+	
+	Write-Verbose ("Logging event. " + $SCRIPT_NAME + " EventID: " + $eventID + " eventType: " + $eventType + " Version:" + $SCRIPT_VERSION + " --> " + $msg)
+	if($level -le $P_TraceLevel)
+	{
+		Write-Host ("Logging event. " + $SCRIPT_NAME + " EventID: " + $eventID + " eventType: " + $eventType + " Version:" + $SCRIPT_VERSION + " --> " + $msg)
+		$g_API.LogScriptEvent($SCRIPT_NAME,$eventID,$eventType, ($msg + "`n" + "Version :" + $SCRIPT_VERSION))
+	}
+}
+
+
+#Start by setting up API object.
+	$P_TraceLevel = $TRACE_VERBOSE
+	$g_Api = New-Object -comObject 'MOM.ScriptAPI'
+	#$g_RegistryStatePath = "HKLM\" + $g_API.GetScriptStateKeyPath($SCRIPT_NAME)
+
+	$dtStart = Get-Date
+	$P_TraceLevel = $traceLevel
+try
+{
+	Import-Module DataProtectionManager
+	$SPTotals = Get-DPMdisk | Measure-Object -prop TotalCapacity,UnallocatedSpace -sum 
+	$PercentFreePool = [single] (($SPTotals[1].Sum / $SPTotals[0].Sum) * 100)
+	$FreePoolGB = [single] ($SPTotals[1].Sum/1GB)
+				$bag = $g_API.CreateTypedPropertyBag($PerformanceDataType)
+				$bag.AddValue("FreePoolPerc", $PercentFreePool)
+				$bag.AddValue("FreePoolGB", $FreePoolGB)
+
+				
+				$bag	#this is the way to return data to OpsMgr
+	If ($traceLevel -eq $TRACE_DEBUG)
+	{
+				$g_API.Return($bag)	#this won't do anything
+	}
+	
+	Log-Event $STOP_EVENT_ID $EVENT_TYPE_SUCCESS ("has completed successfully in " + ((Get-Date)- ($dtstart)).TotalSeconds + " seconds.") $TRACE_INFO
+}
+Catch [Exception] {
+	Log-Event $FAILURE_EVENT_ID $EVENT_TYPE_WARNING ("Main " + $Error) $TRACE_WARNING	
+	write-Verbose $("TRAPPED: " + $_.Exception.GetType().FullName); 
+	Write-Verbose $("TRAPPED: " + $_.Exception.Message); 
+}
