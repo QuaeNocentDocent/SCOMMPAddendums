47cccbb0d458ffb069b3b20c00933e7e0d352c3e
Brandubh <daniele@grandini.com>
2015-02-27 18:26:04 +0100

    Hyper-v Addendum v1 - beta polishing to be done

---
+++ QND.Hyperv2012R2.Addendum/Scripts/Get-VMReplicaStatus.ps1
@@ -0,0 +1,172 @@
+﻿#TO SHOW VERBOSE MESSAGES SET $VerbosePreference="continue"
+#SET ErrorLevel to 5 so show discovery info
+
+#*************************************************************************
+# Script Name - Discover-PrimaryReplicaVM.ps1
+# Author	  -  - Progel spa
+# Version  - 1.0 24.09.2007
+# Purpose     - 
+#               
+# Assumptions - 
+#				
+#               
+# Parameters  - TraceLevel
+#             - ComputerName
+#				- SourceId
+#				- ManagedEntityId
+# Command Line - .\test.ps1 4 "serverName" '{1860E0EB-8C21-41DA-9F35-2FE9343CCF36}' '{1860E0EB-8C21-41DA-9F35-2FE9343CCF36}'
+# If discovery must be added the followinf parameters
+#				SourceId ($ MPElement $ )
+#				ManagedEntityId ($ Target/Id $)
+#
+# Output properties
+#
+# Status
+#
+# Version History
+#	  1.0 21.02.2015 DG First Release
+#     
+#
+# (c) Copyright 2015, Progel spa, All Rights Reserved
+# Proprietary and confidential to Progel srl              
+#
+#*************************************************************************
+
+
+# Get the named parameters
+param([int]$traceLevel=$(throw 'must have a value'))
+
+	[Threading.Thread]::CurrentThread.CurrentCulture = "en-US"        
+	[Threading.Thread]::CurrentThread.CurrentUICulture = "en-US"
+	
+#Constants used for event logging
+$SCRIPT_NAME			= "Get-VMReplicaStatus.ps1"
+$SCRIPT_ARGS = 1
+$SCRIPT_STARTED			= 831
+$PROPERTYBAG_CREATED	= 832
+$SCRIPT_ENDED			= 835
+$SCRIPT_VERSION = "1.0"
+
+#region Constants
+#Trace Level Costants
+$TRACE_NONE 	= 0
+$TRACE_ERROR 	= 1
+$TRACE_WARNING = 2
+$TRACE_INFO 	= 3
+$TRACE_VERBOSE = 4
+$TRACE_DEBUG = 5
+
+#Event Type Constants
+$EVENT_TYPE_SUCCESS      = 0
+$EVENT_TYPE_ERROR        = 1
+$EVENT_TYPE_WARNING      = 2
+$EVENT_TYPE_INFORMATION  = 4
+$EVENT_TYPE_AUDITSUCCESS = 8
+$EVENT_TYPE_AUDITFAILURE = 16
+
+#Standard Event IDs
+$FAILURE_EVENT_ID = 4000		#errore generico nello script
+$SUCCESS_EVENT_ID = 1101
+$START_EVENT_ID = 1102
+$STOP_EVENT_ID = 1103
+
+#TypedPropertyBag
+$AlertDataType = 0
+$EventDataType	= 2
+$PerformanceDataType = 2
+$StateDataType       = 3
+#endregion
+
+#region Helper Functions
+function Log-Params
+{
+	param($Invocation)
+	$line=''
+	foreach($key in $Invocation.BoundParameters.Keys) {$line += "$key=$($Invocation.BoundParameters[$key])  "}
+	Log-Event $START_EVENT_ID $EVENT_TYPE_INFORMATION  ("Starting script. Invocation Name:$($Invocation.InvocationName)`n Parameters`n $line") $TRACE_INFO
+}
+
+
+function Log-Event
+{
+	param($eventID, $eventType, $msg, $level)
+	
+	Write-Verbose ("Logging event. " + $SCRIPT_NAME + " EventID: " + $eventID + " eventType: " + $eventType + " Version:" + $SCRIPT_VERSION + " --> " + $msg)
+	if($level -le $P_TraceLevel)
+	{
+		Write-Host ("Logging event. " + $SCRIPT_NAME + " EventID: " + $eventID + " eventType: " + $eventType + " Version:" + $SCRIPT_VERSION + " --> " + $msg)
+		$g_API.LogScriptEvent($SCRIPT_NAME,$eventID,$eventType, ($msg + "`n" + "Version :" + $SCRIPT_VERSION))
+	}
+}
+
+Function Throw-EmptyDiscovery
+{
+	param($SourceId, $ManagedEntityId)
+
+	$oDiscoveryData = $g_API.CreateDiscoveryData(0, $SourceId, $ManagedEntityId)
+	Log-Event $FAILURE_EVENT_ID $EVENT_TYPE_WARNING "Exiting with empty discovery data" $TRACE_INFO
+	$oDiscoveryData
+	If($traceLevel -eq $TRACE_DEBUG)
+	{
+		#just for debug proposes when launched from command line does nothing when run inside OpsMgr Agent
+		$g_API.Return($oDiscoveryData)
+	}
+}
+
+Function Throw-KeepDiscoveryInfo
+{
+param($SourceId, $ManagedEntityId)
+	$oDiscoveryData = $g_API.CreateDiscoveryData(0,$SourceId,$ManagedEntityId)
+	#Instead of Snapshot discovery, submit Incremental discovery data
+	$oDiscoveryData.IsSnapshot = $false
+	Log-Event $FAILURE_EVENT_ID $EVENT_TYPE_WARNING "Exiting with null non snapshot discovery data" $TRACE_INFO
+	$oDiscoveryData    
+	If($traceLevel -eq $TRACE_DEBUG)
+	{
+		#just for debug proposes when launched from command line does nothing when run inside OpsMgr Agent	
+		$g_API.Return($oDiscoveryData)
+	}
+}
+#endregion
+
+
+#Start by setting up API object.
+	$P_TraceLevel = $TRACE_VERBOSE
+	$g_Api = New-Object -comObject 'MOM.ScriptAPI'
+	#$g_RegistryStatePath = "HKLM\" + $g_API.GetScriptStateKeyPath($SCRIPT_NAME)
+
+	$dtStart = Get-Date
+	$P_TraceLevel = $traceLevel
+	Log-Params $MyInvocation
+
+try
+{
+		if (!(get-Module -Name Hyper-v)) {Import-Module Hyper-v}
+
+	if (!(get-command -Module Hyper-V -Name Get-VM -ErrorAction SilentlyContinue)) {
+		Log-Event $START_EVENT_ID $EVENT_TYPE_WARNING ("Get-VM Commandlet doesn't exist.") $TRACE_WARNING
+		Throw-EmptyDiscovery
+	}
+
+	$vms = Get-VM | where {$_.ReplicationMode.Value__ -ne 0}	#use the enum codes instead of labels 0 = 'None'
+	foreach($vm in $vms) {
+		$replica = Get-VMReplication -VM $vm
+		$replicaAgeHours = ([DateTime]::Now - $replica.LastReplicationTime).TotalHours
+		$bag = $g_api.CreatePropertyBag()
+		$bag.AddValue('VMId',$vm.VMId.ToString())
+		$bag.AddValue('ReplicationMode',$vm.ReplicationMode.value__ ) #to be used in filters, we just monitor primary replica side ==1
+		$bag.AddValue('ReplicationHealthCode',$vm.ReplicationHealth.value__)
+		$bag.AddValue('ReplicationHealth',$vm.ReplicationHealth.ToString())
+		$bag.AddValue('ReplicationStateCode',$vm.ReplicationState.value__)
+		$bag.AddValue('ReplicationState',$vm.ReplicationState.ToString())
+		$bag.AddValue('ReplicaAgeHours',$replicaAgeHours)
+		$bag
+		Log-Event $START_EVENT_ID $EVENT_TYPE_INFO ("$($vm.VMName) has been processed") $TRACE_DEBUG
+	}
+	Log-Event $STOP_EVENT_ID $EVENT_TYPE_SUCCESS ("has completed successfully in " + ((Get-Date)- ($dtstart)).TotalSeconds + " seconds.") $TRACE_INFO
+}
+Catch [Exception] {
+	Log-Event $FAILURE_EVENT_ID $EVENT_TYPE_WARNING ("Main " + $Error) $TRACE_WARNING	
+	write-Verbose $("TRAPPED: " + $_.Exception.GetType().FullName); 
+	Write-Verbose $("TRAPPED: " + $_.Exception.Message); 
+}
